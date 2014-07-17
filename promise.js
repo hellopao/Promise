e4e239e7b38e09631da0e712354d7a6f94460e34
hellopao <qinjia@outlook.com>
2014-07-17 11:03:14 +0800

    uploadfile

--- promise.js
+++ promise.js
@@ -1,77 +1,120 @@
-var Promise = function() {
-    this.state = 'pending';
-    this.thens = [];
-};
+function isType(obj,type) {
+    return {}.toString.call(obj).toLowerCase().replace(/\[object (\w+)\]/,'$1') === type.toLowerCase();
+}
 
-Promise.prototype.then = function(onFulfilled,onRejected) {
-    if (typeof onFulfilled === 'function') {
+
+function Promise() {
+    this.state = "pending";
+    this.thenQueue = [];
+}
+
+Promise.prototype.then = function (onFulfilled, onRejected) {
+    var thenObj = {};
+
+    if (isType(onFulfilled, 'function')) {
+        thenObj.fulfilled = onFulfilled;
     }
 
-    if (typeof onRejected === 'function') {
+    if (isType(onRejected, 'function')) {
+        thenObj.rejected = onRejected;
     }
 
-    //å¦‚æœçŠ¶æ€ä¸æ˜¯æœªå®Œæˆï¼Œå°±æ‰§è¡Œç»“æœ
-    if (this.state !== 'pending') {
-        setImmediate(function () {
-            this._handleThen();
-        }.bind(this));
-    }
+    this.thenQueue.push(thenObj);
 
-    var promise = new Promise();
-
-    return promise;
-};
-
-Promise.prototype._handleThen = function() {
-    
-};
-
-/*
- *æˆåŠŸçš„å›è°ƒ
- */
-Promise.prototype.resolve = function() {
-    this.state = 'fulfiled';
     return this;
 };
 
-/*
- *å¤±è´¥çš„å›è°ƒ
- */
-Promise.prototype.reject = function() {
-    this.state = 'rejected';
-    return this;
-};
+Promise.prototype.resolve = function (data) {
+    if (this.state !== "pending") {
+        return;
+    }
 
-Promise.prototype.when = function() {
+    var thenObj,fn, result;
+
+    while (thenObj = this.thenQueue.shift()) {
+        fn = thenObj.fulfilled;
+        result = fn && fn.call(null, data);
+        if (result && result.constructor === Promise) {
+            this.thenQueue.forEach(function (restThen) {
+                result.then(restThen.fulfilled, restThen.rejected);
+            });
+            return result;
+        } else {
+            data = result;
+        }
+    }
     
 };
 
-/*
- *å°†ä¸€ä¸ªæ–¹æ³•promiseåŒ–
- */
-Promise.prototype.promisefy = function(fn) {
-    //è¦promiseåŒ–çš„æ–¹æ³•
+Promise.prototype.reject = function (data) {
+    if (this.state !== "pending") {
+        return;
+    }
+
+    var thenObj, fn, result;
+
+    while (thenObj = this.thenQueue.shift()) {
+        fn = thenObj.rejected;
+        result = fn && fn.call(null, data);
+        if (result && result.constructor === Promise) {
+            this.thenQueue.forEach(function (restThen) {
+                result.then(restThen.fulfilled, restThen.rejected);
+            });
+            return result;
+        } else {
+            data = result;
+        }
+    }
+};
+
+
+Promise.all = function (promises) {
+    var resultPromise = new Promise();
+
+    var promiseCount = promises.length;
+    var result = [];
+
+    promises.forEach(function (promise,index) {
+        promise.then(function (data) {
+            result[index] = data;
+            promiseCount--;
+            if (promiseCount === 0) {
+                resultPromise.resolve(result);
+            }
+        }, function (err) {
+            resultPromise.reject(err);
+        });
+    });
+
+    return resultPromise;
+};
+
+Promise.promisify = function () {
+    //Òªpromise»¯µÄ·½·¨
     var fn = fn || [].slice.call(arguments).shift();
 
-    return function() {
-        //è¦promiseåŒ–çš„æ–¹æ³•æ¥æ”¶çš„å‚æ•°
+    return function () {
+        //Òªpromise»¯µÄ·½·¨½ÓÊÕµÄ²ÎÊı
         var fnArgs = [].slice.call(arguments);
 
         var promise = new Promise();
-        var handler = function(err) {
+        var handler = function (err,data) {
             if (err) {
-                return promise.reject(err);
+                promise.reject(err);
+            } else {
+                promise.resolve(data);//[].slice.call(arguments, 1));
             }
-            return promise.resolve([].slice.call(arguments,1));
         }
 
-        //ç»™è¦promiseåŒ–çš„æ–¹æ³•æ·»åŠ æ‰§è¡Œå®Œæˆäº‹ä»¶å¤„ç†
+        //¸øÒªpromise»¯µÄ·½·¨Ìí¼ÓÖ´ĞĞÍê³ÉÊÂ¼ş´¦Àí
         fnArgs.push(handler);
-        fn.apply(null,fnArgs);
+        fn.apply(null, fnArgs);
 
         return promise;
     }
 };
 
+
 module.exports = Promise;
+
 
